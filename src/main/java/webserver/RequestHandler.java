7e192c8a49c5f6f1c1e7308d04fb45f563e35b0b
vvhiteboard <vvhiteboard@gmail.com>
2017-01-31 02:48:53 +0900

    push test

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -14,6 +14,7 @@ public class RequestHandler extends Thread {
 
     private Socket connection;
 
+
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
