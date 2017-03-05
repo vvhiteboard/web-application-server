7c51abea631e9d8f4c14586f7e34583f786aae1f
백승재 <77loopin@gmail.com>
2017-03-05 23:58:28 +0900

    [리팩토링4] Request 테스트 코드 추가 1. Request를 테스트 할 수 있는 간단한 테스트 코드 추가 2. pom.xml Mockito 추가

---
+++ src/test/java/model/HttpRequestTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import util.HttpRequestUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by 77loo on 2017-03-05.
+ */
+public class HttpRequestTest {
+    private String testDirectory="./src/test/resources/";
+
+    @Mock
+    HttpRequestUtils httpRequestUtils;
+
+    @Test
+    public void request_GET() throws Exception {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals(request.getMethod(), "GET");
+        assertEquals(request.getPath(), "/user/create");
+        assertEquals(request.getVersion(), "HTTP/1.1");
+    }
+
+    @Test
+    public void test() {
+        Map<String, String> map = new HashMap<>();
+    }
+}
